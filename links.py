630ed070ae88b6bd080ba6ac5799b151a0e13675
Lwasampijja Baker <bubakes2000@gmail.com>
2024-11-22 20:13:35 +0300

    Update links.py

--- links.py
+++ links.py
@@ -1,4 +1,5 @@
 links = [
     "https://projects.propublica.org/nonprofits/organizations/550357039/202411359349309661/full",
+    "https://projects.propublica.org/nonprofits/organizations/161476258/202401069349301215/full",
     "https://projects.propublica.org/nonprofits/organizations/620502739/202401319349302970/full"
 ]
